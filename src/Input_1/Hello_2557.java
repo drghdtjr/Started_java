b79bec32417d4f930aff159074dce3c3b5fbc6ea
drghdtjr <drghdtjr@naver.com>
2023-08-04 21:33:53 +0900

    230804

--- src/Input_1/Hello_2557.java
+++ src/Input_1/Hello_2557.java
@@ -2,6 +2,6 @@ package Input_1;
 
 public class Hello_2557 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Hello World!");
     }
 }
