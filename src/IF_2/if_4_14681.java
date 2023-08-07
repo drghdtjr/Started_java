42d1c9dfa5aa48fec5d477736a1aa27ef7f9ef61
drghdtjr <drghdtjr@naver.com>
2023-08-07 23:00:02 +0900

    230807

--- src/IF_2/if_4_14681.java
+++ src/IF_2/if_4_14681.java
@@ -1,4 +1,24 @@
 package IF_2;
 
+import java.util.Scanner;
+
 public class if_4_14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a > 0 && b > 0) {
+            System.out.println("1");
+
+        } else if (a < 0 && b > 0) {
+            System.out.println("2");
+
+        } else if (a < 0 && b < 0) {
+            System.out.println("3");
+
+        } else if (a > 0 && b < 0) {
+            System.out.println("4");
+
+        }
+    }
 }
--- src/IF_2/if_5_2884.java
+++ src/IF_2/if_5_2884.java
@@ -1,4 +1,20 @@
 package IF_2;
 
+import java.util.Scanner;
+
 public class if_5_2884 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        if(b<45)
+        {
+            System.out.println((a-1)+" "+(b+15));
+            if(a<0){
+                a=23;
+            }
+
+        }
+
+    }
 }
