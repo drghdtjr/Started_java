10c14dd22ab469184b26d38e032c1f849be44fbc
drghdtjr <drghdtjr@naver.com>
2023-08-14 00:08:22 +0900

    230814

--- src/while_3/while_10_2439.java
+++ src/while_3/while_10_2439.java
@@ -1,4 +1,27 @@
 package while_3;
 
+import java.util.Scanner;
+
 public class while_10_2439 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=1;
+        while(i<=n){
+            int gap=n;
+            while(gap>i){
+                System.out.print(" ");
+                gap--;
+            }
+
+            int j=1;
+            while(j<=i){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+    }
 }
--- src/while_3/while_9_2438.java
+++ src/while_3/while_9_2438.java
@@ -1,4 +1,20 @@
 package while_3;
 
+import java.util.Scanner;
+
 public class while_9_2438 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int i=1;
+        while(i<=n){
+            int j=1;
+            while(j<=i){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
 }
