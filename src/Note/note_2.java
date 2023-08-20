2ec493a71055a99034bd3f35c5bf9ebd75cd1283
drghdtjr <drghdtjr@naver.com>
2023-08-20 21:18:39 +0900

    230820

--- src/Note/note_2.java
+++ src/Note/note_2.java
@@ -1,42 +1,46 @@
+//다이아몬드
+
 package Note;
 
 import java.util.Scanner;
 
 public class note_2 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n= sc.nextInt();
-        int i=1;
-        while(i<=n){
-            int j=1;
-            while(j<=n-i){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //상단
+        int i = 1;
+        while (i <= n) {
+            int j = 1;
+            while (j <= n - i) {
                 System.out.print(" ");
                 j++;
             }
-            int x=1;
-            while(x<=i*2-1){
+            int x = 1;
+            while (x <= i * 2 - 1) {
                 System.out.print("*");
                 x++;
             }
             System.out.println();
             i++;
         }
-        int k=1;
-        while(k<=n){
-            int gap=1;
-            while(gap<k){
-                System.out.print("");
-                gap++;
+        //하단
+        int j=1;
+        while(j<n){
+            int k=1;
+            while(k<=j){
+                System.out.print(" ");
+                k++;
             }
-            int z=2*k-1;
-            while
-
-
-
-
-
+            int d=0;
+            while(d<2*(n-j)-1){
+                System.out.print("*");
+                d++;
+            }
             System.out.println();
-            k++;
+            j++;
         }
     }
 }
+
+
