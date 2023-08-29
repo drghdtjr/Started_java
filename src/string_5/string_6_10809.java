28f3595a91e2430baa99df1293dc9fedd571e70e
drghdtjr <drghdtjr@naver.com>
2023-08-29 21:51:57 +0900

    230829

--- src/string_5/string_6_10809.java
+++ src/string_5/string_6_10809.java
@@ -1,4 +1,19 @@
 package string_5;
 
+import java.util.Scanner;
+
 public class string_6_10809 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str= sc.next();
+        int []arr=new int [26];
+        int i=0;
+        while(i<arr.length){
+            arr[i]=-1;
+            i++;
+        }
+        String s= sc.nextLine();
+
+
+    }
 }
