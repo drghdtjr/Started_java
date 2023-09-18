4366d4d23928922ede1b24d3227ff024b135acae
kimhongseok <drghdtjr@naver.com>
2023-09-18 21:19:01 +0900

    230918

--- src/deepening1_6/deepening1_5_1157.java
+++ src/deepening1_6/deepening1_5_1157.java
@@ -1,4 +1,18 @@
 package deepening1_6;
 
+import java.util.Scanner;
+
 public class deepening1_5_1157 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String st= sc.next();
+        int []arr=new int [26];
+        int i=0;
+        while(i<st.length()){
+            if(){
+
+            }
+            i++;
+        }
+    }
 }
