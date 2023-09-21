dcdbad38165b4edafb7ef52c8675bdff0a806f16
kimhongseok <drghdtjr@naver.com>
2023-09-21 22:08:28 +0900

    230921

---
+++ src/Note/note_9.java
@@ -0,0 +1,8 @@
+package Note;
+
+public class note_9 {
+    public static void main(String[] args) {
+        int arr[]={3, 4, 5};
+        System.out.println(arr[4]);
+    }
+}
--- src/deepening1_6/deepening1_6_2941.java
+++ src/deepening1_6/deepening1_6_2941.java
@@ -1,4 +1,28 @@
 package deepening1_6;
 
+import java.util.Scanner;
+
 public class deepening1_6_2941 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int count = 0;
+        int i = 0;
+        while (i < s.length()) {
+            char ch = s.charAt(i);
+            if(ch=='c' && i < s.length() - 1){
+                if(s.charAt(i+1)=='=' || s.charAt(i+1)=='-') {
+                    i++;
+                }
+            }
+
+            count++;
+
+            i++;
+
+        }
+        System.out.println(count);
+    }
 }
+
+
