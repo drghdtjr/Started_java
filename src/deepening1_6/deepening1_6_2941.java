789ec3a88931768d1c81152cbbbbceb2dc5facbf
kimhongseok <drghdtjr@naver.com>
2023-09-22 14:25:51 +0900

    230922

--- src/deepening1_6/deepening1_6_2941.java
+++ src/deepening1_6/deepening1_6_2941.java
@@ -15,11 +15,36 @@ public class deepening1_6_2941 {
                     i++;
                 }
             }
-
+            if(ch=='d'&&i<s.length()-1){
+                if(s.charAt(i+1)=='z'&&i<s.length()-2) {
+                    if (s.charAt(i + 2) == '=') {
+                        i = i + 2;
+                    }
+                }
+                else if(s.charAt(i+1)=='-'){
+                    i++;
+                }
+            }
+            if(ch=='l'&& i<s.length()-1) {
+                if (s.charAt(i + 1) == 'j') {
+                    i++;
+                }
+            }
+            if(ch=='n'&&i<s.length()-1){
+                if(s.charAt(i+1)=='j'){
+                    i++;
+                }
+            } if(ch=='s'&&i<s.length()-1){
+                if(s.charAt(i+1)=='='){
+                    i++;
+                }
+            } if(ch=='z'&&i<s.length()-1){
+                if(s.charAt(i+1)=='='){
+                    i++;
+                }
+            }
             count++;
-
             i++;
-
         }
         System.out.println(count);
     }
--- src/deepening1_6/deepening1_7_1316.java
+++ src/deepening1_6/deepening1_7_1316.java
@@ -1,4 +1,22 @@
 package deepening1_6;
 
+import java.util.Scanner;
+
 public class deepening1_7_1316 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int i=0;
+        int count=0;
+        while(i<n){
+            String st=sc.next();
+
+
+
+
+
+            i++;
+        }
+    }
+
 }
