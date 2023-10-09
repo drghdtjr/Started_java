4406789c1eeb7e61d8228077589f19cd98fe5495
kimhongseok <drghdtjr@naver.com>
2023-10-09 19:48:42 +0900

    231009

--- src/divisor_9/divisor_5_2581.java
+++ src/divisor_9/divisor_5_2581.java
@@ -4,31 +4,52 @@ import java.util.Scanner;
 
 public class divisor_5_2581 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int m= sc.nextInt();
-        int n= sc.nextInt();
-        int arr[]=new int[n-m];
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int arr[]=new int [n+1];
+        int min=n;
         int sum=0;
-        int min=1;
-        while(m<n){
-            int j=2;
-            int a=0;
-            while(j<=m){
-                if(j==m){
-                    arr[a]=m;
-                    a++;
-                }
-                if(m%j==0){
-                    break;
-                }
-                j++;
+        int s=0;
+        while(s< arr.length){
+            arr[s]=0;
+            s++;
+        }
+        while(m<=n){
+            if(prime(m)){
+                sum+=m;
+                arr[m]=m;
+
             }
             m++;
         }
         int k=0;
-        while(k<arr.length){
-            System.out.println(arr[k]);
-        k++;
+        while(k< arr.length){
+            if(arr[k]<min&&arr[k]>0){
+                min=arr[k];
+            }
+            k++;
         }
+        if(sum==0){
+            System.out.println(-1);
+        }
+        else{
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+    }
+    static boolean prime (int i){
+        if(i<2){
+            return false;
+        }
+        int j=2;
+        while(j<i){
+            if(i%j==0){
+                return  false;
+            }
+            j++;
+        }
+        return true;
     }
 }
