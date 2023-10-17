ef50892a424a735ab3579c8babbd1901fedb24c3
kimhongseok <drghdtjr@naver.com>
2023-10-17 22:08:52 +0900

    231017

--- src/Note/note.java
+++ src/Note/note.java
@@ -5,8 +5,22 @@ import java.util.Scanner;
 public class note {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=3;
-        long x=4;
-        System.out.println(n+x);
+        int n= sc.nextInt();
+        int m= sc.nextInt();
+        int arr[]=new int [n];
+        int i=0;
+        int count=0;
+        while(i<n){
+            arr[i]= sc.nextInt();
+            i++;
+        }
+        while(i<n){
+            if(arr[i]%3==0){
+                count++;
+            }
+            i++;
+        }
+
+
     }
 }
