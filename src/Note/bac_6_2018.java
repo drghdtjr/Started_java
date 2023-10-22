6f9cb9d70339fa276345fc07ac26b97a284e2c58
kimhongseok <drghdtjr@naver.com>
2023-10-22 19:31:05 +0900

    231022

---
+++ src/Note/bac_6_2018.java
@@ -0,0 +1,11 @@
+package Note;
+
+import java.util.Scanner;
+
+public class bac_6_2018 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n= sc.nextLong();
+
+    }
+}
--- src/sort_13/sort_6_1427.java
+++ src/sort_13/sort_6_1427.java
@@ -3,12 +3,23 @@ package sort_13;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class sort_6_1427 {
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         String s=br.readLine();
-
-
+        int i=0;
+        long arr[]=new long[s.length()];
+        while(i<s.length()){
+            arr[i]=s.charAt(i)-'0';
+            i++;
+        }
+        Arrays.sort(arr);
+        int k=0;
+        while(k< arr.length){
+            System.out.print(arr.length-k);
+            k++;
+        }
     }
 }
