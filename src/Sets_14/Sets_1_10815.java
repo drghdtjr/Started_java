b33c38c0b3ae26732f2733c91ae8ceadcdf429fd
kimhongseok <drghdtjr@naver.com>
2023-10-29 22:49:11 +0900

    231029

--- src/Sets_14/Sets_1_10815.java
+++ src/Sets_14/Sets_1_10815.java
@@ -1,4 +1,30 @@
 package Sets_14;
 
+import java.util.Scanner;
+
 public class Sets_1_10815 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[]=new int [n];
+        int m= sc.nextInt();
+        int mrr[]=new int [m];
+        int i=0;
+        while(i<n){
+            arr[i]= sc.nextInt();
+
+            i++;
+        }
+        int k=0;
+        while(k<m){
+            mrr[i]= sc.nextInt();
+            k++;
+        }
+        int a=0;
+        while(a<){
+
+        }
+
+
+    }
 }
