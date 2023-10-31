ac14375eda412b8c65021ac81deec9b9675680a8
kimhongseok <drghdtjr@naver.com>
2023-10-31 21:31:09 +0900

    231031

---
+++ src/Note/note_new_11.java
@@ -0,0 +1,12 @@
+package Note;
+
+import java.util.Scanner;
+
+public class note_new_11 {
+    public static void main(String[] args) {
+        //단리
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+
+    }
+}
--- src/Sets_14/Sets_1_10815.java
+++ src/Sets_14/Sets_1_10815.java
@@ -1,36 +1,33 @@
 package Sets_14;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sets_1_10815 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n= sc.nextInt();
-        int m= sc.nextInt();
         int arr_1[]=new int[n];
-        int arr_2[]=new int[m];
         for(int i=0; i<n; i++){
             arr_1[i]= sc.nextInt();
         }
+        int m= sc.nextInt();
+        int arr_2[]=new int[m];
         for(int i=0; i<m; i++){
             arr_2[i]= sc.nextInt();
         }
         int k=0;
         while(k<m){
             int s=0;
+            int count=0;
             while(s<n){
                 if(arr_2[k]==arr_1[s]){
-                    System.out.print("1");
-                }
-                else{
-                    System.out.print("0");
+                    count++;
                 }
                 s++;
             }
-            System.out.println();
+            System.out.print(count+" ");
             k++;
         }
-
     }
-
 }
