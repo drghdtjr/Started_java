36aef13fb33f31ef21e7eaf65d49edab711cc174
kimhongseok <drghdtjr@naver.com>
2023-11-01 19:02:14 +0900

    231101

--- src/Sets_14/Sets_1_10815.java
+++ src/Sets_14/Sets_1_10815.java
@@ -1,33 +1,14 @@
 package Sets_14;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Sets_1_10815 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n= sc.nextInt();
-        int arr_1[]=new int[n];
-        for(int i=0; i<n; i++){
-            arr_1[i]= sc.nextInt();
-        }
-        int m= sc.nextInt();
-        int arr_2[]=new int[m];
-        for(int i=0; i<m; i++){
-            arr_2[i]= sc.nextInt();
-        }
-        int k=0;
-        while(k<m){
-            int s=0;
-            int count=0;
-            while(s<n){
-                if(arr_2[k]==arr_1[s]){
-                    count++;
-                }
-                s++;
-            }
-            System.out.print(count+" ");
-            k++;
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        
+
     }
+
 }
