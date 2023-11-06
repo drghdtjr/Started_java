5e43902b7e61292cedeba381ada33867d93d66fb
kimhongseok <drghdtjr@naver.com>
2023-11-06 23:55:55 +0900

    231106

--- src/Note/note_new_11.java
+++ src/Note/note_new_11.java
@@ -7,6 +7,30 @@ public class note_new_11 {
         //단리
         Scanner sc=new Scanner(System.in);
         int n= sc.nextInt();
+        int m= sc.nextInt();
+        String []arr_n=new String[n];
+        String []arr_m=new String[m];
+        for(int i=0;i<n;i++){
+            arr_n[i]= sc.next();
+        }
+        for(int i=0;i<m;i++){
+            arr_m[i]= sc.next();
+        }
+        int i=0;
+        int count=0;
+        while(i<n){
+            int j=0;
+            while(j<m){
+                if(arr_n[i].equals(arr_m[j])){
+                    count++;
+                    break;
+                }
+                j++;
+            }
+            i++;
+        }
+        System.out.println(count);
+
 
     }
 }
